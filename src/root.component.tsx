b3ed8b356f3fe1970425e5edf05cec4bd85707e3
Jeff Weimer <jeff.weimer@sureapp.com>
2022-02-28 15:28:16 -0800

    update

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import { links } from "./root.helper";
 import { Link } from "@reach/router";
 
+import "./navigation.module.css";
+
 export default function Root(props) {
   return (
     <nav style={{ background: "steelblue", padding: "25px" }}>
       {links.map((link) => {
         return (
-          <Link
-            key={link.href}
-            to={link.href}
-            style={{ color: "white", textDecoration: "none" }}
-          >
+          <Link key={link.href} to={link.href}>
             {link.name}
           </Link>
         );
