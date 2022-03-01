656b50c976f5d4b8ac126392bcfc8cc68095c0c2
Jeff Weimer <jeff.weimer@sureapp.com>
2022-03-01 14:28:03 -0800

    add momo image

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -3,10 +3,30 @@ import { links } from "./root.helper";
 import { Link } from "@reach/router";
 
 import "./navigation.module.css";
+import Momo from "./assets/momo.png";
 
 export default function Root(props) {
   return (
-    <nav style={{ background: "steelblue", padding: "25px" }}>
+    <nav
+      style={{
+        background: "steelblue",
+        padding: "25px",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <img
+        style={{
+          maxWidth: "50px",
+          width: "100%",
+          height: "auto",
+          marginRight: "15px",
+        }}
+        src={Momo}
+        alt="momo"
+      />
+
       {links.map((link) => {
         return (
           <Link key={link.href} to={link.href} replace>
